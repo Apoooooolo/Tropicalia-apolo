84683a665b7b6810432df5b36bbfa0f63e4b3a8c
Apoooooolo <160729976+Apoooooolo@users.noreply.github.com>
2025-03-25 08:52:35 -0300

    2503

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte')
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+    let tamanhoAtualFonte = 1;
 })
