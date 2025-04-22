d7a821a0925e58713f7c9f91ee7cdab1c9a450a9
Apoooooolo <160729976+Apoooooolo@users.noreply.github.com>
2025-04-22 08:22:13 -0300

    lkkl

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
     botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-list');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
     })
+
+
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
@@ -19,13 +21,13 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
+
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
     });
 
     alternaContraste.addEventListener('click', function () {
         document.body.classList.toggle('alto-contraste');
-    });
+    })
 });
