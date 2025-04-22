475b4f5dce460c2ee13cd0612a1944572ac44074
Apoooooolo <160729976+Apoooooolo@users.noreply.github.com>
2025-04-22 08:08:29 -0300

    fztg

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
 
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-    botaoDeAcessibilidade.addEventListener('click',function(){
+    botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-list');
     })
@@ -13,11 +13,11 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click', function(){
+    aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
-    diminuiFonteBotao.addEventListener('click', function(){
+    diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
